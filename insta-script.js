7eafcb1853f4e7383d3d8f3c9193b9f5977ddfd0
Jen <jen.schwabe2@gmail.com>
2021-10-23 15:44:21 -0500

    Initial comment toggling work

--- insta-script.js
+++ insta-script.js
@@ -3,3 +3,21 @@
 function f(number) {
     console.log("open draft " + number);
 }
+
+function toggleComments() {
+  console.log("toggling comments visibility");
+  var comments = document.getElementsByClassName("comments");
+  var button = document.getElementById("load-comments")
+  if (button.innerHTML === "Show comments") {
+    button.innerHTML = "Hide comments";
+  } else {
+    button.innerHTML = "Show comments";
+  }
+  for (var i = 0; i < comments.length; i++) {
+    if (comments[i].style.display === "block") {
+      comments[i].style.display = "none";
+    } else {
+      comments[i].style.display = "block";
+    }
+  }
+}
